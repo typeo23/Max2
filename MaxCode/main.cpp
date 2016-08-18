f118d6d58580bc5b47e138b243107dc68714aabb
typeo23 <itayabar@post.tau.ac.il>
2016-08-17 22:27:58 -0700

    Initial Commit

---
+++ MaxCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MaxCode
+//
+//  Created by itay barel on 8/17/16.
+//  Copyright © 2016 itay barel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
